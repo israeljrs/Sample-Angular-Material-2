00a70dba6cdb267f33bad4dfb2e94da0def2b883
Israel Junior <israeljrs@gmail.com>
2017-09-20 16:27:22 -0300

    add material design.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  private showMessage() {
+    swal('Material Design', 'Exemplo finalizado com sucesso !!!', 'success');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MdButtonModule, MdToolbarModule, MdCardModule, MdIconModule, MdMenuModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdToolbarModule,
+    MdCardModule,
+    MdIconModule,
+    MdMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
